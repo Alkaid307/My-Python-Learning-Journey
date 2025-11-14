b91d070174cf33fefda262664f28416948c28ef1
Alkaid307 <alkaid_Roche@qq.com>
2025-11-14 11:06:15 +0800

    Update 题目1_等差列表求和.py

--- 题目1_等差列表求和.py
+++ 题目1_等差列表求和.py
@@ -5,13 +5,16 @@ Created on Fri Oct 24 09:07:44 2025
 @author: 陈倩菲
 """
 
+# 根据起始项a₁和步长值，可以对等差数列求和，即a₁+ a₂+……+an的值
 n = int(input("请输入n的值（n为整数）："))
-# 输入n值
+d = int(input("请输入步长d的值："))
+a = int(input("请输入起始项a₁的值："))
+# 输入n、d、a₁值
 sum_range = 0
 # 设置储存列表之和的变量，并预设为0
-for x in range(1,n*2,2):
-# 由于题目中的等差列表步长值为2，因此设置结束值为n*2
-    sum_range += x
+for x in range(1,n+1):
+    an = a+(x-1)*d
+    sum_range += an
 # 将遍历的值累加到预设的变量
 print("a₁+ a₂+……+an的值为",sum_range)
 # 打印最后的结果
